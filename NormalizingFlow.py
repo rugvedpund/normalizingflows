45b0da771af1bbbc79d20fdb7d9ce9cf1ae620c5
Rugved Pund <rugvedpund@gmail.com>
2023-01-25 20:46:44 -0500

    fix NF.py

--- NormalizingFlow.py
+++ NormalizingFlow.py
@@ -70,7 +70,7 @@ class NormalizingFlow:
         plt.colorbar()
         plt.suptitle('Likelihood')
         plt.show()
-    def plotHistograms(self,data,modeidxToPlot,other=None,means=None, t21=None,logscale=False,niterationSteps=1):
+    def plotHistograms(self,data,modeidxToPlot,other=None,means=None, t21=None,logscale=False,niterationSteps=1, fname=None):
         self._precomputeIterations(data,niterationSteps)
         data=self._toTensor(data)
         other=self._toTensor(other) if other is not None else None
@@ -104,7 +104,8 @@ class NormalizingFlow:
                     plt.ylabel(f'mode {j+1}')
             # plt.suptitle(f'iteration {i}')
             plt.tight_layout()
-            plt.savefig(f"{i}_"+"_".join([str(m) for m in modeidxToPlot]))
+            plt.suptitle(fname)
+            plt.savefig(f"{fname}_{i}_"+"_".join([str(m) for m in modeidxToPlot])+'.png')
             plt.show()
     def plotCovMat(self,data,logscale=False, niterationSteps=1):
         self._precomputeIterations(data,niterationSteps)
@@ -158,19 +159,137 @@ class FlowAnalyzer(NormalizingFlow):
         super().__init__(nocuda=nocuda,loadPath=loadPath)
     def precomputeIterations(self,data,niterationSteps):
         self._precomputeIterations(data,niterationSteps)
-    def set_fg(self,fg, sigma=None, one_over_f=False,subsample=1):
-        if sigma is None:
-            self.fg=fg
+    def set_fg(self, fg, sigma, chromatic, galcut, noPCA, subsample, noise_K, noiseSeed, combineSigma):
+        self.fg=fg
+        self.sigma=sigma
+        self.chromatic=chromatic
+        self.galcut=galcut
+        self.noPCA=noPCA
+        self.subsample=subsample
+        self.noise_K=noise_K
+        self.noiseSeed=noiseSeed
+        self.combineSigma=combineSigma
+        
+        self.noise=self.generate_noise(self.fg,self.noise_K,self.subsample,self.noiseSeed)
+        self.fg_noisy=self.fg+self.noise
+        self.fgsmooth=self.smooth(self.fg_noisy,self.sigma,self.chromatic)
+        
+        if self.galcut is not None:
+            self.fgsmooth_cut=self.galaxy_cut(self.fgsmooth,self.galcut)
         else:
-            self.fg=smooth(fg,sigma,one_over_f)
-        self.nfreqs,self.ndata=self.fg.shape
-        self.proj_fg,self.proj_fgmeans=get_projected_data(self.fg,self.fg.mean(axis=1))
-        self.proj_fg=self.proj_fg[::subsample]
-    def set_t21(self,t21):
-        _,self.proj_t21=get_projected_data(self.fg,t21) 
-        self.ref10x=self.proj_fgmeans+10*self.proj_t21
-        self.ref1x=self.proj_fgmeans+self.proj_t21
+            self.fgsmooth_cut=self.fgsmooth
+            
+        if self.noPCA: 
+            self.data,self.fgmeansdata=self.get_data_noPCA(self.fgsmooth_cut, vector=self.fgsmooth_cut.mean(axis=1))
+        else:
+            self.data,self.fgmeansdata=self.get_data_PCA(self.fgsmooth_cut, vector=self.fgsmooth_cut.mean(axis=1))
+        
+        if self.combineSigma is not None:
+            self.fgsmooth2=self.smooth(self.fg,self.combineSigma,self.chromatic)
+            if self.galcut is not None:
+                self.fgsmooth_cut2=self.galaxy_cut(self.fgsmooth2,self.galcut)
+            else:
+                self.fgsmooth_cut2=self.fgsmooth2
+            self.fgsmoothcut_combineSigma=np.vstack([self.fgsmooth_cut,self.fgsmooth_cut2])
+            if self.noPCA: 
+                self.data,self.fgmeansdata=self.get_data_noPCA(self.fgsmoothcut_combineSigma, vector=self.fgsmoothcut_combineSigma.mean(axis=1))
+            else:
+                self.data,self.fgmeansdata=self.get_data_PCA(self.fgsmoothcut_combineSigma, vector=self.fgsmoothcut_combineSigma.mean(axis=1))
+
+            
+        assert self.data.shape[0]>self.data.shape[1]
+        assert self.subsample>=3
+        print(f'subsampling {self.subsample}')
+        self.train_data=self.data[::self.subsample,:]
+        self.validate_data=self.data[1::self.subsample,:]
+        self.test_data=self.data[2::self.subsample,:]
+        print('done! self.train_data,validate_data and test_data ready')
+
+    def generate_noise(self,fg,noise_K,subsample,seed=0):
+        nfreq,ndata=fg.shape
+        np.random.seed(seed)
+        noise_sigma=noise_K*np.sqrt(ndata/subsample)
+        noise=np.random.normal(0,noise_sigma,(nfreq,ndata))
+        return noise
     
+    def smooth(self,fg,sigma,chromatic):
+        print(f'smoothing with {sigma} deg, and chromatic {chromatic}')
+        fgsmooth=np.zeros_like(fg)
+        nfreq,ndata=fg.shape
+        for f in range(nfreq):
+            sigma_f=sigma*(10.0/(f+1)) if chromatic else sigma
+            fgsmooth[f,:]=hp.sphtfunc.smoothing(fg[f,:],sigma=np.deg2rad(sigma_f))
+        return fgsmooth
+
+    def galaxy_cut(self,fg,b_min):
+        print(f'doing galcut for b_min={b_min} deg')
+        _,npix=fg.shape
+        nside=np.sqrt(npix/12).astype(int)
+        col_min = np.pi/2 - np.deg2rad(b_min)
+        col_max = np.pi/2 + np.deg2rad(b_min)
+        mask_pix = hp.query_strip(nside, col_min, col_max, inclusive=False)
+        cutfg = np.delete(fg, mask_pix, axis=1)
+        return cutfg
+
+    def get_data_PCA(self, data, vector=None):
+        """
+        data should be of shape (nfreqs,ndata)
+        vector should be of shape (nfreqs)
+        out is of shape (ndata,nfreqs) - because torch needs it that way
+        """
+        print('doing PCA')
+        assert data.shape[0]<data.shape[1]
+        # if vector is not None: assert vector.shape==data.shape[0]
+        cov=np.cov(data)
+        eva,eve=np.linalg.eig(cov)
+        s=np.argsort(np.abs(eva))[::-1] 
+        eva,eve=np.real(eva[s]),np.real(eve[:,s])
+        proj_data=eve.T@(data-data.mean(axis=1)[:,None]) #subtract mean and project
+        rms=np.sqrt(proj_data.var(axis=1))
+        out=(proj_data/rms[:,None]).T #divide by rms
+        out=np.random.permutation(out)
+        if vector is None: 
+            return out
+        else:
+            return out, (eve.T@vector)/rms
+
+    def get_data_noPCA(self, data, vector=None):
+        print('not doing PCA')
+        assert data.shape[0]<data.shape[1]
+        rms=np.sqrt(data.var(axis=1))
+        out=((data - data.mean(axis=1)[:,None])/rms[:,None]).T
+        out=np.random.permutation(out)
+        if vector is None:
+            return out
+        else:
+            return out, vector/rms
+    
+    def set_t21(self, t21, include_noise):
+        if self.combineSigma is not None:
+            print(f'combining sigma {self.sigma} with combineSigma {self.combineSigma}')
+            if include_noise: 
+                t21+=self.noise.mean(axis=1)
+                print('including noise in t21')
+            self.t21=np.hstack([t21,t21])
+            print(self.t21.shape, self.fgsmoothcut_combineSigma.shape)
+            print(f'projecting t21 according to noPCA={self.noPCA}')
+            if self.noPCA:
+                _,self.t21data=self.get_data_noPCA(self.fgsmoothcut_combineSigma, self.t21)
+            else:
+                _,self.t21data=self.get_data_PCA(self.fgsmoothcut_combineSigma, self.t21)
+            print('done! self.t21data ready')
+        else:
+            if include_noise: 
+                t21+=self.noise.mean(axis=1)
+                print('including noise in t21')
+            self.t21=t21
+            print(f'projecting t21 according to noPCA={self.noPCA}')
+            if self.noPCA:
+                _,self.t21data=self.get_data_noPCA(self.fgsmooth_cut, self.t21)
+            else:
+                _,self.t21data=self.get_data_PCA(self.fgsmooth_cut, self.t21)
+            print('done! self.t21data ready')
+            
         
 def load_ulsa_t21_tcmb_freqs():
     root='/home/rugved/Files/LuSEE/luseepy/Drive/'
@@ -180,11 +299,11 @@ def load_ulsa_t21_tcmb_freqs():
     tcmb=np.ones_like(t21)*2.73
     return fg, t21, tcmb, freqs
 
-def smooth(fg,sigma,one_over_f):
+def smooth(fg,sigma,chromatic):
     fgsmooth=np.zeros_like(fg)
     nfreq,_=fg.shape
     for f in range(nfreq):
-        sigma_f=sigma*(10.0/(f+1)) if one_over_f else sigma
+        sigma_f=sigma*(10.0/(f+1)) if chromatic else sigma
         fgsmooth[f,:]=hp.sphtfunc.smoothing(fg[f,:],sigma=np.deg2rad(sigma_f))
     return fgsmooth
 
@@ -208,5 +327,4 @@ def get_projected_data(data, vector=None):
         return out
     else:
         return out, (eve.T@vector)/rms
-
 
