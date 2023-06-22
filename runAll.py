1c95594234f5db4f9801ac822069c887f7668687
Rugved Pund <rugvedpund@gmail.com>
2023-06-22 10:06:29 -0400

    fix cleanT21

--- runAll.py
+++ runAll.py
@@ -19,7 +19,7 @@ parser.add_argument('--noise', type=float, default=0.0, required=False)
 parser.add_argument('--noiseSeed', type=int, default=0, required=False)
 parser.add_argument('--subsampleSigma', type=float, default=2.0, required=False)
 
-parser.add_argument('--cleanT21', action='store_true')
+parser.add_argument('--noisyT21', action='store_true')
 parser.add_argument('--gainFluctuationLevel', type=float, default=0.0, required=False)
 parser.add_argument('--gFdebug', type=int, default=0, required=False)
 parser.add_argument('--append')
@@ -59,36 +59,39 @@ flow.set_fg(fg=fg,sigma=args.sigma,chromatic=args.chromatic,
 print('setting t21...')
 freqs=np.arange(1,51)
 t21=lusee.mono_sky_models.T_DarkAges_Scaled(freqs,nu_rms=14,nu_min=16.4,A=0.04)
-flow.set_t21(t21, include_noise=not args.cleanT21)
+flow.set_t21(t21, include_noise=args.noisyT21)
 if args.retrain: flow.train(flow.train_data, flow.validate_data, nocuda=False, savePath=fname,retrain=True)
 
-npoints=100
+npoints=50
 # kwargs={'amin':0.0,'amax':150.0,'wmin':1.0,'wmax':40.0,'nmin':1.0,'nmax':40.0}
 # kwargs={'amin':0.0,'amax':3.0,'wmin':10.0,'wmax':20.0,'nmin':10.0,'nmax':20.0}
 # kwargs={'amin':0.5,'amax':1.5,'wmin':11.0,'wmax':17.0,'nmin':15.0,'nmax':18.0}
 kwargs={'amin':0.01,'amax':100.0,'wmin':11.0,'wmax':17.0,'nmin':15.0,'nmax':18.0,
          'logspace':True}
 
-#3D corner
-samples,t21_vs=nf.get_t21vs(npoints,**kwargs)
-t21_vsdata=flow.proj_t21(t21_vs,include_noise=True)
-likelihood=flow.get_likelihood(t21_vsdata,args.freqFluctuationLevel,args.DA_factor, debugfF=True) #REMEMBER TO SWITCH debugfF
-#save
-lname=nf.get_lname(args,plot='all')
-print(f'saving corner likelihood results to {lname}')
-np.savetxt(lname,np.column_stack([samples,likelihood]),header='amp,width,numin,loglikelihood')
+# #3D corner
+# print('getting 3d likelihood...')
+# samples,t21_vs=nf.get_t21vs(npoints,**kwargs)
+# t21_vsdata=flow.proj_t21(t21_vs,include_noise=True)
+# likelihood=flow.get_likelihood(t21_vsdata,args.freqFluctuationLevel,args.DA_factor, debugfF=True) #REMEMBER TO SWITCH debugfF
+# #save
+# lname=nf.get_lname(args,plot='all')
+# print(f'saving corner likelihood results to {lname}')
+# np.savetxt(lname,np.column_stack([samples,likelihood]),header='amp,width,numin,loglikelihood')
 
-#2D corner
-for vs in ['WvA','NvA','WvN']:
-    samples2d,t21_vs2d=nf.get_t21vs2d(npoints,vs,**kwargs)
-    t21_vsdata2d=flow.proj_t21(t21_vs2d,include_noise=True)
-    likelihood2d=flow.get_likelihood(t21_vsdata2d,args.freqFluctuationLevel,args.DA_factor, debugfF=True)
-    lname=nf.get_lname(args,plot=vs)
-    print(f'saving 2d likelihood results to {lname}')
-    np.savetxt(lname,np.column_stack([samples2d,likelihood2d]),header='amp,width,numin,loglikelihood')
+# #2D corner
+# for vs in ['WvA','NvA','WvN']:
+#     print(f'getting 2d likelihood for {vs}...')
+#     samples2d,t21_vs2d=nf.get_t21vs2d(npoints,vs,**kwargs)
+#     t21_vsdata2d=flow.proj_t21(t21_vs2d,include_noise=True)
+#     likelihood2d=flow.get_likelihood(t21_vsdata2d,args.freqFluctuationLevel,args.DA_factor, debugfF=True)
+#     lname=nf.get_lname(args,plot=vs)
+#     print(f'saving 2d likelihood results to {lname}')
+#     np.savetxt(lname,np.column_stack([samples2d,likelihood2d]),header='amp,width,numin,loglikelihood')
 
 #1D
 for vs in ['A','W','N']:
+    print(f'getting 1d likelihood for {vs}...')
     samples1d,t21_vs1d=nf.get_t21vs1d(npoints,vs,**kwargs)
     t21vsdata1d=flow.proj_t21(t21_vs1d,include_noise=True)
     likelihood1d=flow.get_likelihood(t21vsdata1d,args.freqFluctuationLevel,args.DA_factor,debugfF=True)
