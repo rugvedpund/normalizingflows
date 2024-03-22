455d21fda260cc50a5bf7f03d0b818049923ba02
Rugved Pund <rugvedpund@gmail.com>
2024-03-21 22:45:32 -0400

    add figtext log to 3dplot

--- 3dplot.py
+++ 3dplot.py
@@ -23,7 +23,10 @@ cosmicdawn = True if args.fgFITS == "gsm16.fits" else False
 truths = [1, 20, 67.5] if cosmicdawn else [1, 14, 16.4]
 ranges = (
     # [(0,5), (19, 21), (66, 69)] if cosmicdawn else [(0, 10), (1, 30), (1, 30)]
-    [(0,5), (19, 21), (66, 69)] if cosmicdawn else [(0, 2), (10, 20), (10, 20)]
+    # [(0,5), (19, 21), (66, 69)] if cosmicdawn else [(0, 2), (10, 20), (10, 20)]
+    [(0, 5), (19, 21), (66, 69)]
+    if cosmicdawn
+    else [(0.5, 1.5), (13.5, 14.5), (16, 16.6)]
     # [(0,5), (19, 21), (66, 69)] if cosmicdawn else [(0, 2), (1, 30), (1, 30)]
 )
 fg = "CD" if cosmicdawn else "DA"
@@ -35,29 +38,33 @@ labels = {
 cornerkwargs = {
     "show_titles": True,
     "levels": [1 - np.exp(-0.5), 1 - np.exp(-2)],
-    "bins": 60,
-    "range": ranges,
+    "bins": 50,
+    # "range": ranges,
     "labels": [r"A", r"$\nu_{\rm rms}$", r"$\nu_{\rm min}$"],
     "plot_datapoints": True,
-    "truths":truths,
+    "truths": truths,
     # "no_fill_contours":True,
     # "plot_contours":False,
     # "pcolor_kwargs":{"cmap":"viridis"},
     # "hist_kwargs":{"density": True},
 }
-suptitle=rf"{fg} for {labels[args.combineSigma]},"
-if args.SNRpp is not None: suptitle+=f' SNR={args.SNRpp:.0e}'
-else: suptitle+=f' Noise={args.noise:.0e} K'
-suptitle+='\n'
 
 ##---------------------------------------------------------------------------##
 # plot samples and likelihoods
 
 
 s, ll = nf.get_samplesAndLikelihood(args, plot="all")
+print("Total number of samples: ", s.shape)
 fig = corner.corner(s, weights=nf.exp(ll), **cornerkwargs)
 # corner.overplot_lines(fig, truths, color="k", ls="--", lw=1)
-plt.suptitle(suptitle)
+plt.figtext(
+    0.8,
+    0.7,
+    parser.argstostring(args),
+    bbox=dict(facecolor="gray", alpha=0.5),
+    fontsize="xx-small",
+    ma="center",
+)
 plt.show()
 
 # savefig should be available via the matplotlib gui
